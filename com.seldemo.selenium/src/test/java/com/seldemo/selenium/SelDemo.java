c9b5c054267ce85bd6bbc97d86e8a431eba2fea8
mahadevi <kolkarmahadevi@gmail.com>
2020-06-30 21:04:06 +0530

    maven project changes

---
+++ com.seldemo.selenium/src/test/java/com/seldemo/selenium/SelDemo.java
@@ -0,0 +1,12 @@
+package com.seldemo.selenium;
+
+import org.testng.annotations.Test;
+
+public class SelDemo {
+	@Test
+	public void tetsel()
+	{
+		System.out.println("selenium maven project");
+	}
+
+}
